798b44d48fd863f079e7262d6f2d606ddae3ac19
fossildog117 <quantumbiology@hotmail.co.uk>
2016-03-30 12:56:34 +0100

    Update cw3.hs

--- cw3.hs
+++ cw3.hs
@@ -1,13 +1,16 @@
 type Graph = [(Integer, [Integer])]
 
+-- Gets vertices 1 to x of a given graph
 getVertices :: Graph -> [Integer]
 getVertices [] = []
 getVertices (v:vs) = [fst v] ++ getVertices vs
 
+-- Gets list of edges
 getListOfEdges :: Graph -> [[Integer]]
 getListOfEdges [] = []
 getListOfEdges (g:gs) = [snd g] ++ getListOfEdges gs
 
+-- Gets a list of edges connected to a vertex n 
 getAdjacentVertices :: Int -> Graph -> [Integer]
 getAdjacentVertices n g = getListOfEdges g !! n
 
