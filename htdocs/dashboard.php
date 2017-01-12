a08ed90f1b8fca72a691e4d4e03e23f673e16a0e
fossildog117 <nathanliu@eduroam-int-dhcp-97-240-43.ucl.ac.uk>
2017-01-12 18:53:12 +0000

    website

---
+++ htdocs/dashboard.php
@@ -0,0 +1,28 @@
+
+<?php
+require('db.php');
+/**
+ * Created by PhpStorm.
+ * User: nathanliu
+ * Date: 12/01/2017
+ * Time: 15:39
+ */
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Dashboard - Secured Page</title>
+    <link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+<div class="form">
+    <p>Welcome to Dashboard.</p>
+    <p><a href="index.php">Home</a><p>
+    <p><a href="insert.php">Insert Snippet</a></p>
+    <p><a href="view.php">View Snippets</a><p>
+    <p><a href="logout.php">Logout</a></p>
+</div>
+</body>
+</html>
